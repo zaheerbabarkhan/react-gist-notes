da5843f941b42d97114ae1b324f7d7e2313a343a
zaheerbabarkhan <zaheerbaberkhan@gmail.com>
2024-05-23 14:45:13 +0500

    create gist page added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import MainLayout from './layout/MainLayout'
 import HomePage from './pages/HomePage'
-import SingleGistPublic from './pages/SingleGistPublic'
+import SingleGistPublic from './pages/SingleGistPublicPage'
+import CreateGistPage from './pages/CreateGistPage'
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
     <Route path='/gists' element={<HomePage />} />
     <Route path='/gists/:id' element={<SingleGistPublic />}/>
+    <Route path='/gists/create' element={<CreateGistPage />}/>
   </Route>
 ))
 function App() {
--- src/components/pageHeader/pageHeader.tsx
+++ src/components/pageHeader/pageHeader.tsx
@@ -1,9 +1,13 @@
-import { useState } from "react"
+import React, { useState } from "react"
 import "./pageHeader.scss"
 import { IoIosList, IoMdGrid } from "react-icons/io"
 
+interface PageHeaderProps {
+    displayViewButtons?: boolean
+    headerText: string
+}
 
-const PageHeader = () => {
+const PageHeader: React.FC<PageHeaderProps> = ({ displayViewButtons = true, headerText }) => {
     const [gridActive, setGridActive] = useState(false);
     const [listActive, setListActive] = useState(true);
     const activateAlternateView = () => {
@@ -13,17 +17,19 @@ const PageHeader = () => {
     return (
         <div className="page-header">
             <div className="page-heading">
-                <p>Public Gists</p>
+                <p>{headerText}</p>
             </div>
-            <div className="view-buttons">
-                <button className={`grid-button ${gridActive ? "grid-active": ""}`} onClick={activateAlternateView}>
-                    <IoMdGrid size={27} className="grid-icon" />
-                </button>
-                <button className={`list-button ${listActive ? "list-active" : ""}`} onClick={activateAlternateView}>
-                    <IoIosList size={27} className="list-icon" />
-                </button>
+            {
+                displayViewButtons && <div className="view-buttons">
+                    <button className={`grid-button ${gridActive ? "grid-active" : ""}`} onClick={activateAlternateView}>
+                        <IoMdGrid size={27} className="grid-icon" />
+                    </button>
+                    <button className={`list-button ${listActive ? "list-active" : ""}`} onClick={activateAlternateView}>
+                        <IoIosList size={27} className="list-icon" />
+                    </button>
 
-            </div>
+                </div>
+            }
         </div>
     )
 }
